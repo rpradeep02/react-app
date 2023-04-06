0c9a20649180397ac56b842aaf425904b5bc3e7b
rpradeep02 <kumarrpradeep123@gmail.com>
2023-04-06 22:04:03 +0530

    learned conditional in react

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,16 @@
 function ListGroup() {
+  let items = ["madurai", "bglr", "chennai", "cbe"];
+  items = [];
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>no items found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
