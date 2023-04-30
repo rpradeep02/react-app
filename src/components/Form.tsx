03f053d3dae39e17459dee65d8c85cb3bac19a4e
rpradeep02 <kumarrpradeep123@gmail.com>
2023-05-01 00:15:17 +0530

    learned form validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,19 +1,87 @@
-import React from "react";
+import React, { FormEvent, useRef } from "react";
+import { useForm } from "react-hook-form";
 
+// const Form = () => {
+//   const nameRef = useRef<HTMLInputElement>(null);
+//   const ageRef = useRef<HTMLInputElement>(null);
+//   const person = { name: "", age: 0 };
+
+//   const handleSubmit = (event: FormEvent) => {
+//     event.preventDefault();
+//     if (nameRef.current !== null) person.name = nameRef.current.value;
+//     if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+//     console.log(person);
+//   };
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <div className="mb-3">
+//         <label htmlFor="name" className="form-label">
+//           Name
+//         </label>
+//         <input ref={nameRef} id="name" type="text" className="form-control" />
+//       </div>
+//       <div className="mb-3">
+//         <label htmlFor="age" className="form-label">
+//           Age
+//         </label>
+//         <input ref={ageRef} id="age" type="number" className="form-control" />
+//       </div>
+//       <button className="btn btn-primary" type="submit">
+//         Submit
+//       </button>
+//     </form>
+//   );
+// };
+
+interface FormData {
+  name: string;
+  age: number;
+}
 const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+  const onSubmit = (data: FieldValues) => console.log(data);
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input
+          {...register("name", { required: true, minLength: 3 })}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+        {errors.name?.type == "required" && (
+          <p className="text-danger">the name field is required</p>
+        )}
+        {errors.name?.type == "minLength" && (
+          <p className="text-danger">the name should be minimum 3 character</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input
+          {...register("age", { required: true, maxLength: 3, minLength: 1 })}
+          id="age"
+          type="number"
+          className="form-control"
+        />
+        {errors.age?.type == "required" && (
+          <p className="text-danger">the age field is required</p>
+        )}
+        {errors.age?.type == "minLength" && (
+          <p className="text-danger">the age should be minimum 1 character</p>
+        )}
+        {errors.age?.type == "maxLength" && (
+          <p className="text-danger">the age should be maximum 1 character</p>
+        )}
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
